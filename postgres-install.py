41ab34540f7eae653f8958baf2add1eb59e1f041
Incept1on <incept1onnn999@gmail.com>
2025-04-13 12:51:45 +0300

    add parsing for the memory usage and add function that returns the desired ip to intall postgres to

--- postgres-install.py
+++ postgres-install.py
@@ -1,5 +1,6 @@
 import argparse
 import subprocess
+import re
 
 def generate_inventory(ip_list):
     with open("inventory.txt", "w") as f:
@@ -13,6 +14,31 @@ def run_ansible(playbook, limit=None):
         cmd += ["--limit", limit]
     return subprocess.run(cmd)
 
+def choose_least_load():
+    with open("freemem_report.txt", "r") as f:
+        data = {}
+        for line in f.readlines():
+            splitted = line.split(": ")
+            data[splitted[0]] = splitted[2]
+
+        dataCleaned = {}
+
+        for ip, memValue in data.items():
+            digitalsMem = re.findall(r'\d+', memValue)
+            dataCleaned[ip] = int(digitalsMem[0])
+        
+        sortedDict = dict(sorted(dataCleaned.items(), key=lambda item: item[1]))
+
+        lessMemIp = list(sortedDict.items())[0]
+
+        return str(lessMemIp[0])
+        
+
+
+
+
+
+
 def main():
     parser = argparse.ArgumentParser(prog='postgres-install')
     parser.add_argument("hosts", nargs="+", help="Comma-separated list of IPs or hostnames")
@@ -21,9 +47,16 @@ def main():
     # generate_inventory(args.hosts)
 
     print("Gathering facts about conjunction of services...")
-    run_ansible("gather_facts.ansible.yaml")
+
+    # run_ansible("gather_facts.ansible.yaml")
+
+    choose_least_load()
+
+
 
     # print("Then less busy host will be chosen and postgress will be installed...")
+
+
 
 
 
